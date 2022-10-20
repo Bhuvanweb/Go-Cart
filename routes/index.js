8c68e1e7099b5c284b8803b684e4d5348a2fc41d
Bhuvanendran B <webhuvan@gmail.com>
2022-10-20 22:30:13 +0530

    Layout set in layout branch

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+let products=[
+  {name:"Apple",
+  category:"fruit",
+  description:"It's the best food",
+  image:"https://healthjade.com/wp-content/uploads/2017/10/apple-fruit.jpg",
+  price:"400"
+}
+]
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { products , admin:false});
 });
 
 module.exports = router;
