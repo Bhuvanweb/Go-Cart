286a294c961ffa11192bba29d0b227152c39ae07
Bhuvanendran B <webhuvan@gmail.com>
2022-11-02 23:16:38 +0530

    sign up & login are set

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const {engine}=require('express-handlebars')
+const session=require('express-session')
 
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
@@ -23,6 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
+app.use(session({secret:'Key',cookie:{maxAge:600000}}))
 db.connect((err)=>{
   if(err){
     console.log(`Connection Error ${err}`);
--- routes/user.js
+++ routes/user.js
@@ -6,8 +6,9 @@ var userHelpers=require('../helpers/user-helpers')
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
+  let user=req.session.user
   productHelpers.getAllProducts().then((products)=>{
-    res.render('user/view-products', { products });
+    res.render('user/view-products', { products,user });
   })
 });
 
@@ -19,7 +20,25 @@ router.get('/signup',(req,res)=>{
 })
 
 router.post('/signup',(req,res)=>{
-  userHelpers.doSignup(req.body)
+  userHelpers.doSignup(req.body).then((response)=>{
+    console.log(response);
+  })
+})
+router.post('/login',(req,res)=>{
+  userHelpers.doLogin(req.body).then((response)=>{
+    if(response.status){
+      req.session.loggedIn=true
+      req.session.user=response.user
+      res.redirect('/')
+    }else{
+      res.redirect('/login')
+    }
+  })
+})
+
+router.get('/logout',(req,res)=>{
+  req.session.destroy()
+  res.redirect('/')
 })
 
 module.exports = router;
