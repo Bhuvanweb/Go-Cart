dcaa4c9f0fe0928e196effb1b823154c7bc9114e
Bhuvanendran B <webhuvan@gmail.com>
2022-11-30 12:38:40 +0530

    Add-product button starts working now

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,54 +1,74 @@
-var db=require('../config/connection')
-var collection=require('../config/collections')
-var bcrypt=require('bcrypt')
-const ObjectId=require('mongodb').ObjectId
-module.exports={
-    doSignup:(userData)=>{
-        return new Promise(async(resolve,reject)=>{
-            userData.Password=await bcrypt.hash(userData.Password,10)
-            db.get().collection(collection.USER_COLLECTION).insertOne(userData).then((data)=>{
-                resolve(data.insertedId)
-            })
+var db = require("../config/connection");
+var collection = require("../config/collections");
+var bcrypt = require("bcrypt");
+const ObjectId = require("mongodb").ObjectId;
+module.exports = {
+  doSignup: (userData) => {
+    return new Promise(async (resolve, reject) => {
+      userData.Password = await bcrypt.hash(userData.Password, 10);
+      db.get()
+        .collection(collection.USER_COLLECTION)
+        .insertOne(userData)
+        .then((data) => {
+          resolve(data.insertedId);
+        });
+    });
+  },
+  doLogin: (userData) => {
+    return new Promise(async (resolve, reject) => {
+      let loginStatus = false;
+      let response = {};
+      let user = await db
+        .get()
+        .collection(collection.USER_COLLECTION)
+        .findOne({ Email: userData.Email });
+      if (user) {
+        bcrypt.compare(userData.Password, user.Password).then((status) => {
+          if (status) {
+            console.log(`Login success`);
+            response.user = user;
+            response.status = true;
+            resolve(response);
+          } else {
+            console.log(`Login failed`);
+            resolve({ status: false });
+          }
+        });
+      } else {
+        console.log(`Login failed`);
+        resolve({ status: false });
+      }
+    });
+  },
+  addToCart: (proId, userId) => {
+    return new Promise(async (resolve, reject) => {
+      let userCart = await db
+        .get()
+        .collection(collection.CART_COLLECTION)
+        .findOne({ user: ObjectId(userId) });
+      if (userCart) {
+        db
+        .get()
+        .collection(collection.CART_COLLECTION)
+        .updateOne({user:ObjectId(userId)},{
+                $push:{products:ObjectId(proId)}
+        }).then((response)=>{
+            resolve()
         })
-    },
-    doLogin:(userData)=>{
-        return new Promise(async(resolve,reject)=>{
-            let loginStatus=false
-            let response={}
-            let user=await db.get().collection(collection.USER_COLLECTION).findOne({Email:userData.Email})
-            if(user){
-                bcrypt.compare(userData.Password,user.Password).then((status)=>{
-                    if(status){
-                        console.log(`Login success`);
-                        response.user=user
-                        response.status=true
-                        resolve(response)
-                    }else{
-                        console.log(`Login failed`);
-                        resolve({status:false})
-                    }
-                })
-            }else{
-                console.log(`Login failed`);
-                resolve({status:false})
-            }
-        })
-    },
-    addToCart:(proId,userId)=>{
-        return new Promise(async(resolve,reject)=>{
-            let userCart=await db.get().collection(collection.CART_COLLECTION).findOne({user:ObjectId(userId)})
-            if(userCart){
-
-            }else{
-                let cartObj={
-                    user:ObjectId(userId),
-                    products:[ObjectId(proId)]
-                }
-                db.get().collection(collection.CART_COLLECTION).insertOne(cartObj).then((response)=>{
-                    resolve()
-                })
-
-            }
-        })
-    }
-}
+      } else {
+        let cartObj = {
+          user: ObjectId(userId),
+          products: [ObjectId(proId)],
+        };
+        console.log(`cartObj: ${cartObj}`);
+        db.get()
+          .collection(collection.CART_COLLECTION)
+          .insertOne(cartObj)
+          .then((response) => {
+            resolve();
+          });
+        // console.log(cartObj);
+      }
+    });
+  },
+};
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var productHelpers=require('../helpers/product-helpers')
 var userHelpers=require('../helpers/user-helpers')
-let varifyLogin=(req,res,next)=>{
+var varifyLogin=(req,res,next)=>{
   if(req.session.loggedIn){
     next()
   }else{
@@ -23,8 +23,8 @@ router.get('/login',(req,res)=>{
   if(req.session.user){
     res.redirect('/')
   }else{
-    res.render('user/login',{"loginErr":req.session.userLoginErr})
-    req.session.userLoginErr=false;
+    res.render('user/login',{"loginErr":req.session.LoginErr})
+    req.session.LoginErr=false;
   }
 })
 router.get('/signup',(req,res)=>{
@@ -34,19 +34,19 @@ router.get('/signup',(req,res)=>{
 router.post('/signup',(req,res)=>{
   userHelpers.doSignup(req.body).then((response)=>{
     console.log(response);
+    req.session.loggedIn=true
     req.session.user=response
-    req.session.user.loggedIn=true
     res.redirect('/')
   })
 })
 router.post('/login',(req,res)=>{
   userHelpers.doLogin(req.body).then((response)=>{
     if(response.status){
+      req.session.loggedIn=true
       req.session.user=response.user
-      req.session.user.loggedIn=true
       res.redirect('/')
     }else{
-      req.session.userLoginErr="Invalid username or Password"
+      req.session.LoginErr="Invalid username or Password"
       res.redirect('/login')
     }
   })
@@ -60,7 +60,8 @@ router.get('/cart',varifyLogin,(req,res)=>{
   res.render('user/cart')
 })
 router.get('/add-to-cart/:id',varifyLogin,(req,res)=>{
-  console.log(req.params.id,req.session.user._id);
+  // console.log(req.body);
+  console.log(req.params.id);
   userHelpers.addToCart(req.params.id,req.session.user._id).then(()=>{
     res.redirect('/')
   })
