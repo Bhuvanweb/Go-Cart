d91eea86414510330801faea9a1926e231b2630f
Bhuvanendran B <webhuvan@gmail.com>
2022-12-01 12:15:33 +0530

    getCartProducts in userHelpers.js with aggregation

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -71,4 +71,24 @@ module.exports = {
       }
     });
   },
+  getCartProducts:(userId)=>{
+    return new Promise(async(resolve,reject)=>{
+      let cartItems=await db.get().collection(collection.CART_COLLECTION).aggregate([
+        {$match:{user:ObjectId(userId)}},
+        {$lookup:
+          {from:collection.PRODUCT_COLLECTION,
+          let:{proList:"$products"},
+          pipeline:[
+            {$match:{
+              $expr:{
+                $in:["$_id","$$proList"]
+              }
+            }}
+          ],
+          as:'cartItems'
+        }}
+      ]).toArray()
+      resolve(cartItems[0].cartItems)
+    })
+  }
 };
--- routes/user.js
+++ routes/user.js
@@ -56,7 +56,9 @@ router.get('/logout',(req,res)=>{
   req.session.user=null
   res.redirect('/')
 })
-router.get('/cart',varifyLogin,(req,res)=>{
+router.get('/cart',varifyLogin,async(req,res)=>{
+  let products=await userHelpers.getCartProducts(req.session.user._id)
+  console.log(products);
   res.render('user/cart')
 })
 router.get('/add-to-cart/:id',varifyLogin,(req,res)=>{
